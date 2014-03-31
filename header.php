73371fe2e0ed6fd658c50e5768e4e0cb4fae9154
gvinter <galen.vinter@gmail.com>
2014-03-30 20:41:20 -0400

    move markup into theme. still static in header/footer/index

--- header.php
+++ header.php
@@ -26,78 +26,58 @@
 
 <body>
 
- 	<header>
- 		<div class="container">
-	    <p class="site-logo">Front-end Dev Weekly</p>
-	   </div>
+ 	<!--[if lt IE 7]>
+        <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+
+  <!-- Header -->
+  <header>
+    <div class="container">
+      <p class="site-logo"><span>Front-end Dev</span> Weekly</p>
+     </div>
   </header>
 
+  <!-- Banner -->
   <div class="banner">
-  	<div class="container">
-  		<div class="banner-inner">
-  			<h1 class="title">Hand picked news, trends, and inspiration each week.</h1>
-  			<p class="subtitle">Coming soon, a curated, front-end web developer newsletter will begin. It will include hand picked content from around the web including news, trends, tips, and inspiration. No spam. Just meaningful content for the world's web developers that will help you stay current. This isn't an RSS feed. It's an emailed delivered to you once per week. Free.</p>
-  		</div>
-  	</div>
+    <div class="container">
+      <div class="banner-inner">
+        <h1 class="title"><span>Hand picked</span> <strong>front-end developer news, trends and inspiration</strong> <span class="end-of-title">each week.</span></h1>
+        <p class="subtitle"><span>Vital content for front-end web developers that will keep you current.</span> Emailed to you each week, free.</p>
+      </div>
+    </div>
+    <div class="banner-signup">
+      <div class="container">
+
+        <!-- MailChimp Signup Form -->
+        <div id="mc_embed_signup">
+          <form class="form-inline" action="http://listentoo.us7.list-manage2.com/subscribe/post?u=11df2122a2a2c5c3fc46242c5&amp;id=c6dafce16b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+              <label class="sr-only" for="mce-EMAIL">Email Address </label>
+              <input type="email" value="" name="EMAIL" class="form-control required email" id="mce-EMAIL" placeholder="Your email address" />
+              <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button btn btn-success">
+              
+              <div id="mce-responses" class="clear">
+                <div class="response" id="mce-error-response" class="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" class="mce-success-response" style="display:none"></div>
+              </div>
+              
+              <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+              <div style="position: absolute; left: -5000px;">
+                <input type="text" name="b_11df2122a2a2c5c3fc46242c5_c6dafce16b" value="">
+              </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div class="banner-menu">
+      <div class="container">
+        <nav>
+          <ul>
+            <li><a href="#">Last Week</li>
+            <li><a href="#">All Issues</a></li>
+            <li><a href="#">By Category</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   </div>
-
-	<div class="container">
-
-	  <div class="stream">
-
-	  	<div class="row">
-	    	<p class="issue-title"><span class="issue-num-date col-sm-2 col-sm-pull-1">Issue 1 <br/> April 1, 2014</span> <span class="issue-title col-sm-8 col-sm-pull-1">Latest and greatest title for the newsletter. This will be the subject of the emails too.</span></p>
-	    </div>
-
-	    <div class="row issue-commentary">
-				<span class="issue-commentary-meta col-sm-1"><span class="headshot"></span><br/>Wrap-up</span>
-				<p class="col-sm-8">Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-	    </div>
-
-	    <div class="row">
-	    
-		    <section class="col-lg-10 news">
-
-		    	<h3>News</h3>
-
-		    	<article class="row">
-		      	<span class="col-sm-1 hidden-xs">News</span>
-		      	<h4 class="col-sm-8">This is going to be the title of the content being shown below.</h4>
-		      	<p class="col-sm-8 col-sm-push-1">Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-		      	<a href="#" class="col-sm-8 col-sm-push-1">http://galenvinter.com</a>
-		      </article>
-
-					<article class="row">
-		      	<span class="col-sm-1 hidden-xs">News</span>
-		      	<h4 class="col-sm-8">This is going to be the title of the content being shown below.</h4>
-		      	<p class="col-sm-8 col-sm-push-1">Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-		      	<a href="#" class="col-sm-8 col-sm-push-1">http://galenvinter.com</a>
-		      </article>
-
-		    </section>
-
-		    <section class="col-lg-10 frameworks">
-
-		    	<h3>Frameworks</h3>
-
-					<article class="row">
-		      	<span class="col-sm-1 hidden-xs">News</span>
-		      	<h4 class="col-sm-8">This is going to be the title of the content being shown below.</h4>
-		      	<p class="col-sm-8 col-sm-push-1">Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-		      	<a href="#" class="col-sm-8 col-sm-push-1">http://galenvinter.com</a>
-		      </article>
-
-					<article class="row">
-		      	<span class="col-sm-1 hidden-xs">News</span>
-		      	<h4 class="col-sm-8">This is going to be the title of the content being shown below.</h4>
-		      	<p class="col-sm-8 col-sm-push-1">Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-		      	<a href="#" class="col-sm-8 col-sm-push-1">http://galenvinter.com</a>
-		      </article>
-
-		    </section>
-
-		  </div>
-
-	  </div><!-- /stream -->
-	  
-	</div> <!-- /container -->
--- index.php
+++ index.php
@@ -1,13 +1,113 @@
 <?php get_header(); ?>
 
-	<?php if ( have_posts() ) : ?>
+<?php if ( have_posts() ) : ?>
 
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
-		<?php endwhile; ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+		<?php get_template_part( 'content', get_post_format() ); ?>
+	<?php endwhile; ?>
 
-	<?php else : ?>
-		<?php get_template_part( 'content', 'none' ); ?>
-	<?php endif; ?>
+<?php else : ?>
+	<?php get_template_part( 'content', 'none' ); ?>
+<?php endif; ?>
+
+<!-- Main Content -->
+<div class="container">
+
+  <div class="stream">
+
+    <div class="row">
+      <h2 class="issue-title"><span class="issue-num-date col-sm-2 col-xs-10">Issue 1 <br/> April 1, 2014</span> <span class="issue-title col-sm-8 col-xs-10">Latest and greatest title for the newsletter. This will be the subject of the emails too.</span></h2>
+    </div>
+
+    <div class="row issue-commentary">
+      <div class="issue-commentary-meta col-xs-2">
+        <span class="headshot"><img src="wp-content/themes/fedw/img/galen.jpg"></span>
+        <span class="galen">
+          Galen Vinter
+          <a href="http://twitter.com/gvinter">@gvinter</a>
+        </span>
+      </div>
+      <p class="col-xs-8">Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+    </div>
+
+    <div class="row stream-sections">
+    
+      <section class="col-lg-10 news">
+
+        <h3>News</h3>
+
+        <article class="row">
+          <h4 class="col-sm-10">This is going to be the title of the content being shown below.</h4>
+          <p class="col-sm-10">Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+          <a href="#" class="col-sm-10">http://galenvinter.com</a>
+        </article>
+
+        <article class="row">
+          <h4 class="col-sm-10">This is going to be the title of the content being shown below.</h4>
+          <p class="col-sm-10">Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+          <a href="#" class="col-sm-10">http://galenvinter.com</a>
+        </article>
+
+      </section>
+
+      <section class="col-lg-10 frameworks">
+
+        <h3>Frameworks</h3>
+
+        <article class="row">
+          <h4 class="col-sm-10">This is going to be the title of the content being shown below.</h4>
+          <p class="col-sm-10">Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+          <a href="#" class="col-sm-10">http://galenvinter.com</a>
+        </article>
+
+        <article class="row">
+          <h4 class="col-sm-10">This is going to be the title of the content being shown below.</h4>
+          <p class="col-sm-10">Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+          <a href="#" class="col-sm-10">http://galenvinter.com</a>
+        </article>
+
+      </section>
+
+      <section class="col-lg-10 tutorials">
+
+        <h3>Tutorials</h3>
+
+        <article class="row">
+          <h4 class="col-sm-10">This is going to be the title of the content being shown below.</h4>
+          <p class="col-sm-10">Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+          <a href="#" class="col-sm-10">http://galenvinter.com</a>
+        </article>
+
+        <article class="row">
+          <h4 class="col-sm-10">This is going to be the title of the content being shown below.</h4>
+          <p class="col-sm-10">Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+          <a href="#" class="col-sm-10">http://galenvinter.com</a>
+        </article>
+
+      </section>
+
+      <section class="col-lg-10 dev-tools">
+
+        <h3>Dev Tools</h3>
+
+        <article class="row">
+          <h4 class="col-sm-10">This is going to be the title of the content being shown below.</h4>
+          <p class="col-sm-10">Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+          <a href="#" class="col-sm-10">http://galenvinter.com</a>
+        </article>
+
+        <article class="row">
+          <h4 class="col-sm-10">This is going to be the title of the content being shown below.</h4>
+          <p class="col-sm-10">Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+          <a href="#" class="col-sm-10">http://galenvinter.com</a>
+        </article>
+
+      </section>
+
+    </div>
+
+  </div><!-- /stream -->
+  
+</div> <!-- /container -->
 
 <?php get_footer(); ?>
